e2d6d4db6940a2178013602f9fac37462208f647
irudnyts <yegor.rudnytskiy@gmail.com>
2015-09-08 16:59:21 +0200

    Add namespace to the external function get.hist.quote from tseries

--- R/get_prices_from_tickers.R
+++ R/get_prices_from_tickers.R
@@ -33,39 +33,45 @@ get_prices_form_tickers <- function(..., start, end,
     if(retclass == "list") {
         prices <- list()
         for(ticker in tickers) {
-            prices[[ticker]] <- get.hist.quote(instrument = ticker,
-                                               start = start, end = end,
-                                               quote = quote,
-                                               provider = "yahoo",
-                                               compression = "d",
-                                               retclass = "zoo")
+            prices[[ticker]] <- tseries::get.hist.quote(instrument = ticker,
+                                                        start = start,
+                                                        end = end,
+                                                        quote = quote,
+                                                        provider = "yahoo",
+                                                        compression = "d",
+                                                        retclass = "zoo")
         }
     } else if(retclass == "zoo") {
         prices <- NULL
         for(ticker in tickers) {
             if(is.null(prices)) {
-                prices <- get.hist.quote(instrument = ticker, start = start,
-                                         end = end, quote = quote,
-                                         provider = "yahoo",
-                                         compression = "d", retclass = "zoo")
+                prices <- tseries::get.hist.quote(instrument = ticker,
+                                                  start = start,
+                                                  end = end, quote = quote,
+                                                  provider = "yahoo",
+                                                  compression = "d",
+                                                  retclass = "zoo")
             } else {
-                prices <- merge(prices, get.hist.quote(instrument = ticker,
-                                                       start = start, end = end,
-                                                       quote = quote,
-                                                       provider = "yahoo",
-                                                       compression = "d",
-                                                       retclass = "zoo"))
+                prices <- merge(prices,
+                                tseries::get.hist.quote(instrument = ticker,
+                                                        start = start,
+                                                        end = end,
+                                                        quote = quote,
+                                                        provider = "yahoo",
+                                                        compression = "d",
+                                                        retclass = "zoo"))
             }
         }
         colnames(prices) <- tickers
     } else if(retclass == "data.frame") {
         prices <- NULL
         for(ticker in tickers) {
-            current_prices <- get.hist.quote(instrument = ticker, start = start,
-                                             end = end, quote = quote,
-                                             provider = "yahoo",
-                                             compression = "d",
-                                             retclass = "zoo")
+            current_prices <- tseries::get.hist.quote(instrument = ticker,
+                                                      start = start, end = end,
+                                                      quote = quote,
+                                                      provider = "yahoo",
+                                                      compression = "d",
+                                                      retclass = "zoo")
             if(is.null(prices)) {
                 prices <- data.frame(date = time(current_prices), prices =
                                             coredata(current_prices))
