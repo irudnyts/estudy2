c9291c2cb00c989a21852a78da80c784206d2946
irudnyts <yegor.rudnytskiy@gmail.com>
2015-09-08 10:46:11 +0200

    Add documentation for get_prices_from_tickers

--- R/get_prices_from_tickers.R
+++ R/get_prices_from_tickers.R
@@ -1,11 +1,35 @@
+#' Daily Prices of Securities
+#'
+#' Returns the daily Open or Close prices between \code{start} and \code{end}
+#' date for given tickers.
+#'
+#' This function uses the function \code{get.hist.quote} form packege
+#' \code{tseries}. The providor set automatically to Yahoo Finance. The function
+#' returns the data in different class-conrainers: list of zoo's, zoo, or
+#' data.frame.
+#'
+#' @param ... character vectors indicating the tickers (should be valid in Yahoo
+#' Finance).
+#' @param start the object of the Date class, which specifies the first date of
+#' observed time period.
+#' @param end the object of the Date class, which specifies the last date of
+#' observed time period.
+#' @param quote a character indicating the type of the price (Open or Close).
+#' The default value is Open
+#' @param retclass a character specifying the return class. "list", "data.frame"
+#' , or "zoo", by default is "list"
+#' @return the data about prices in given class container: "list", "data.frame",
+#'  or "zoo".
+#'
+#' @seealso \code{\link[tseries]{get.hist.quote}}
 get_prices_form_tickers <- function(..., start, end,
                                     quote = c("Open", "Close"),
                                     retclass = c("list", "data.frame", "zoo")) {
-
-    # validity of arguments
     quote <- match.arg(quote)
     retclass <- match.arg(retclass)
     tickers <- c(...)
+    # for avoiding check inside the loop, the if/else is done outside the loop,
+    # that is why so many repetition of the code. Done for optimality and speed.
     if(retclass == "list") {
         prices <- list()
         for(ticker in tickers) {
