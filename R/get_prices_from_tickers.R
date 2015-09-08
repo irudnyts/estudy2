2956a0d25c19cec78047d84b79d66b0585f2f75c
irudnyts <yegor.rudnytskiy@gmail.com>
2015-09-08 10:28:36 +0200

    Add different return class.

--- R/get_prices_from_tickers.R
+++ R/get_prices_from_tickers.R
@@ -1,29 +1,60 @@
 get_prices_form_tickers <- function(..., start, end,
-                                    quote = c("Open", "Close")) {
+                                    quote = c("Open", "Close"),
+                                    retclass = c("list", "data.frame", "zoo")) {
 
     # validity of arguments
     quote <- match.arg(quote)
+    retclass <- match.arg(retclass)
     tickers <- c(...)
-    result <- NULL
-
-    for(ticker in tickers) {
-        if(is.null(result)) {
-            result <- get.hist.quote(instrument = ticker, start = start,
-                                     end = end, quote = quote,
-                                     provider = "yahoo",
-                                     compression = "d", retclass = "zoo")
-        } else {
-            result <- merge(result, get.hist.quote(instrument = ticker,
-                                                   start = start,
-                                                   end = end, quote = quote,
-                                                   provider = "yahoo",
-                                                   compression = "d",
-                                                   retclass = "zoo"))
+    if(retclass == "list") {
+        prices <- list()
+        for(ticker in tickers) {
+            prices[[ticker]] <- get.hist.quote(instrument = ticker,
+                                               start = start, end = end,
+                                               quote = quote,
+                                               provider = "yahoo",
+                                               compression = "d",
+                                               retclass = "zoo")
         }
+    } else if(retclass == "zoo") {
+        prices <- NULL
+        for(ticker in tickers) {
+            if(is.null(prices)) {
+                prices <- get.hist.quote(instrument = ticker, start = start,
+                                         end = end, quote = quote,
+                                         provider = "yahoo",
+                                         compression = "d", retclass = "zoo")
+            } else {
+                prices <- merge(prices, get.hist.quote(instrument = ticker,
+                                                       start = start, end = end,
+                                                       quote = quote,
+                                                       provider = "yahoo",
+                                                       compression = "d",
+                                                       retclass = "zoo"))
+            }
+        }
+        colnames(prices) <- tickers
+    } else if(retclass == "data.frame") {
+        prices <- NULL
+        for(ticker in tickers) {
+            current_prices <- get.hist.quote(instrument = ticker, start = start,
+                                             end = end, quote = quote,
+                                             provider = "yahoo",
+                                             compression = "d",
+                                             retclass = "zoo")
+            if(is.null(prices)) {
+                prices <- data.frame(date = time(current_prices), prices =
+                                            coredata(current_prices))
+            } else {
+                prices <- merge(prices, data.frame(date = time(current_prices),
+                                                   prices =
+                                                      coredata(current_prices)),
+                                by = "date")
+            }
+        }
+        colnames(prices) <- c("date", tickers)
     }
-    time(result) <- as.Date(time(result))
-    colnames(result) <- tickers
-    return(result)
+    return(prices)
 }
 
 
