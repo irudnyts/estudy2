7ba4be2e6e61b62bb569255aa170537617266215
irudnyts <yegor.rudnytskiy@gmail.com>
2015-10-07 16:51:47 +0200

    Add right enviroment to lock bindning

--- R/constants.R
+++ R/constants.R
@@ -1,4 +1,5 @@
 # consts:
-k_q1 <- qnorm(1 - 0.10/2)#; lockBinding("k_q1", globalenv())
-k_q2 <- qnorm(1 - 0.05/2)#; lockBinding("k_q2", globalenv())
-k_q3 <- qnorm(1 - 0.01/2)#; lockBinding("k_q3", globalenv())
+k_q1 <- qnorm(1 - 0.10/2); lockBinding("k_q1", environment())
+k_q2 <- qnorm(1 - 0.05/2); lockBinding("k_q2", environment())
+k_q3 <- qnorm(1 - 0.01/2); lockBinding("k_q3", environment())
+
