9094b7abbff4064827564c73ee00b2ca6852d8c9
irudnyts <yegor.rudnytskiy@gmail.com>
2015-10-09 09:53:05 +0200

    Change the name of constants

--- R/constants.R
+++ R/constants.R
@@ -1,5 +1,5 @@
-# consts:
-k_q1 <- qnorm(1 - 0.10/2); lockBinding("k_q1", environment())
-k_q2 <- qnorm(1 - 0.05/2); lockBinding("k_q2", environment())
-k_q3 <- qnorm(1 - 0.01/2); lockBinding("k_q3", environment())
+# constants:
+const_q1 <- qnorm(1 - 0.10/2); lockBinding("const_q1", environment())
+const_q2 <- qnorm(1 - 0.05/2); lockBinding("const_q2", environment())
+const_q3 <- qnorm(1 - 0.01/2); lockBinding("const_q3", environment())
 
