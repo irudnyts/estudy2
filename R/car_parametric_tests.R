863b2bc06eacbce5c5a72c99866fc8894683dd71
irudnyts <yegor.rudnytskiy@gmail.com>
2015-12-07 14:42:36 +0100

    Fix minor bugs

--- R/car_parametric_tests.R
+++ R/car_parametric_tests.R
@@ -6,17 +6,19 @@ car_parametric_tests <- function() {
 #' @export
 car_lamb <- function(list_of_returns, car_start, car_end) {
     daily_lamb_statistics <- lamb(list_of_returns, car_start, car_end)
-    percentage <- mean(daily_lamb_statistics[3])
-    statistic <- sum(daily_lamb_statistics[5], na.rm = T) /
-        sqrt(length(daily_lamb_statistics))
+    percentage <- mean(daily_lamb_statistics[, 3])
+    statistic <- sum(daily_lamb_statistics[, 5], na.rm = T) /
+        sqrt(nrow(daily_lamb_statistics))
     significance <- ""
-    significance <- ifelse(test = abs(statistics) >= const_q1,
-                           yes = "*")
-    significance <- ifelse(test = abs(statistics) >= const_q2,
-                           yes = "**")
-    significance <- ifelse(test = abs(statistics) >= const_q3,
-                           yes = "***")
+    significance <- ifelse(test = abs(statistic) >= const_q1,
+                           yes = "*", no = "")
+    significance <- ifelse(test = abs(statistic) >= const_q2,
+                           yes = "**", no = "")
+    significance <- ifelse(test = abs(statistic) >= const_q3,
+                           yes = "***", no = "")
     result <- list(car_start = car_start, car_end = car_end,
                    percentage = percentage, statistic = statistic,
+                   number_of_days = length(daily_lamb_statistics),
                    significance = significance)
+    return(result)
 }
