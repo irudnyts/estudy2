50a9692ac52f1e27e1636735f3429ba7e263fdbc
irudnyts <yegor.rudnytskiy@gmail.com>
2016-02-12 17:57:16 +0100

    Add function car_brown_warner_1985

--- R/car_parametric_tests.R
+++ R/car_parametric_tests.R
@@ -26,3 +26,28 @@ car_lamb <- function(list_of_returns, car_start, car_end, percentage) {
                    significance = significance)
     return(result)
 }
+
+#' @export
+car_brown_warner_1985 <- function(list_of_returns, car_start, car_end,
+                                  percentage) {
+    daily_lamb_statistics <- brown_warner_1985(list_of_returns, car_start, car_end)
+    daily_lamb_statistics_tidy <- daily_lamb_statistics[daily_lamb_statistics[, 3] > percentage &
+                                                            daily_lamb_statistics[, 3] > 0, ]
+    percentage <- mean(daily_lamb_statistics_tidy[, 3])
+
+    browser()
+    statistic <- sum(daily_lamb_statistics_tidy[, 5], na.rm = T) /
+        sqrt(nrow(daily_lamb_statistics_tidy))
+    significance <- ""
+    significance <- ifelse(test = abs(statistic) >= const_q1,
+                           yes = "*", no = "")
+    significance <- ifelse(test = abs(statistic) >= const_q2,
+                           yes = "**", no = "")
+    significance <- ifelse(test = abs(statistic) >= const_q3,
+                           yes = "***", no = "")
+    result <- list(car_start = car_start, car_end = car_end,
+                   average_percentage = percentage, statistic = statistic,
+                   number_of_days = nrow(daily_lamb_statistics_tidy),
+                   significance = significance)
+    return(result)
+}
