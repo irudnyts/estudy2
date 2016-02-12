8707756487986a8855fc0e371d3a8569605c025e
irudnyts <yegor.rudnytskiy@gmail.com>
2016-02-12 17:49:20 +0100

    * Add parameter percentage for controlling missed-data companies * Delete weekends * Fix the number of days * Change the name of average percentage

--- R/car_parametric_tests.R
+++ R/car_parametric_tests.R
@@ -4,15 +4,15 @@ car_parametric_tests <- function() {
 }
 
 #' @export
-car_lamb <- function(list_of_returns, car_start, car_end) {
+car_lamb <- function(list_of_returns, car_start, car_end, percentage) {
     daily_lamb_statistics <- lamb(list_of_returns, car_start, car_end)
-    percentage <- mean(daily_lamb_statistics[, 3]) # exclude weekends
+    daily_lamb_statistics_tidy <- daily_lamb_statistics[daily_lamb_statistics[, 3] > percentage &
+                                                        daily_lamb_statistics[, 3] > 0, ]
+    percentage <- mean(daily_lamb_statistics_tidy[, 3])
 
-
-
-
-    statistic <- sum(daily_lamb_statistics[, 5], na.rm = T) /
-        sqrt(nrow(daily_lamb_statistics))
+    browser()
+    statistic <- sum(daily_lamb_statistics_tidy[, 5], na.rm = T) /
+        sqrt(nrow(daily_lamb_statistics_tidy))
     significance <- ""
     significance <- ifelse(test = abs(statistic) >= const_q1,
                            yes = "*", no = "")
@@ -21,9 +21,8 @@ car_lamb <- function(list_of_returns, car_start, car_end) {
     significance <- ifelse(test = abs(statistic) >= const_q3,
                            yes = "***", no = "")
     result <- list(car_start = car_start, car_end = car_end,
-                   percentage = percentage, statistic = statistic,
-                   number_of_days = length(daily_lamb_statistics), # must be nrow()
+                   average_percentage = percentage, statistic = statistic,
+                   number_of_days = nrow(daily_lamb_statistics_tidy),
                    significance = significance)
-    # return(result)
-    return("DO NOT USE")
+    return(result)
 }
