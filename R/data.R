34e4014bb4137b8f501740003e33baca70fd8862
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2018-02-02 17:14:49 +0100

    Edit documentation of data

--- R/data.R
+++ R/data.R
@@ -1,12 +1,11 @@
-#' Stock prices of 9 major insurance companies from 2000-01-01 to 2002-01-01
+#' Stock prices of nine major insurance companies from 2000-01-03 to 2002-01-01
 #'
-#' A list, which contains 9 elements, each of which is \code{zoo} object,
-#' containing daily Close stock prices of 9 major insurance companies from
-#' 2000-01-01 to 2002-01-01. The code for generatin this dataset can be seen in
-#' the documentation of \code{get_prices_from_tickers} function. The variables
-#' are as follows:
+#' A \code{zoo} object of nine columns containing daily Close stock prices of
+#' nine major insurance companies from 2000-01-03 to 2002-01-01. The code for
+#' generatin this dataset can be seen in the documentation of
+#' \code{get_prices_from_tickers} function.
 #'
-#' @format A list of 9 \code{zoo} elements:
+#' @format A \code{zoo} object of nine columns:
 #' \itemize{
 #'   \item ALV.DE
 #'   \item CS.PA
@@ -20,15 +19,24 @@
 #' }
 "prices"
 
-#' Rates of returns of 9 major insurance companies from 2000-01-01 to 2002-01-01
+#' Prices of of ESTX50 EUR P index from 2000-01-03 to 2001-12-31
 #'
-#' A list, which contains 9 elements, each of which is \code{zoo} object,
-#' containing daily rates of returns of corresponding 9 major insurance
-#' companies from 2000-01-01 to 2002-01-01. The code for generatin this dataset
-#' can be seen in the documentation of \code{get_rates_from_prices} function.
-#' The variables are as follows:
+#' A \code{zoo} object containing daily prices of ESTX50 EUR P index from
+#' 2000-01-03 to 2001-12-31. The code for generatin this dataset can be seen in
+#' the documentation of \code{get_prices_from_tickers} function.
 #'
-#' @format A list of 10 \code{zoo} elements:
+#' @format A \code{zoo} object.
+"prices_indx"
+
+#' Rates of returns of nine major insurance companies from 2000-01-04 to
+#' 2002-01-01
+#'
+#' A \code{zoo} object of nine columns containing  daily rates of returns of
+#' nine major insurance companies from 2000-01-04 to 2002-01-01. The code for
+#' generatin this dataset can be seen in the documentation of
+#' \code{get_rates_from_prices} function.
+#'
+#' @format A \code{zoo} object of nine columns:
 #' \itemize{
 #'   \item ALV.DE
 #'   \item CS.PA
@@ -42,26 +50,24 @@
 #' }
 "rates"
 
-#' Rates of returns of ESTX50 EUR P index from 2000-01-01 to 2002-01-01
+#' Rates of returns of ESTX50 EUR P index from 2000-01-04 to 2001-12-31
 #'
-#' A list of one element, which is presented by \code{zoo} class and contains
-#' daily rates of returns of ESTX50 EUR P index from 2000-01-01 to 2002-01-01.
-#' The code for generatin this dataset can be seen in the documentation of
-#' \code{apply_market_model} function.
+#' A \code{zoo} object containing daily rates of returns of ESTX50 EUR P index
+#' from 2000-01-04 to 2001-12-31. The code for generatin this dataset can be
+#' seen in the documentation of \code{get_rates_from_prices} function.
 #'
-#' @format A list of one \code{zoo} object.
-#'
+#' @format A \code{zoo} object.
 "rates_indx"
 
-#' Returns of 9 major insurance companies from 2000-01-01 to 2002-01-01
+#' Returns of nine major insurance companies from 2000-01-04 to 2001-12-29
 #'
-#' A list of length 9, elements of which are objects of the class
-#' \code{returns}. The list contains all necessary returns of 9 major insurance
-#' companies from 2000-01-01 to 2002-01-01. The code for generatin this dataset
-#' can be seen in the documentation of \code{apply_market_model} function. The
-#' variables are as follows:
+#' A list of length nine, elements of which are objects of the class
+#' \code{returns}. The list contains all necessary returns of nine major
+#' insurance companies from 2000-01-04 to 2001-12-29. The code for generatin
+#' this dataset can be seen in the documentation of \code{apply_market_model}
+#' function.
 #'
-#' @format A list of 10 \code{zoo} elements:
+#' @format A list of nine \code{zoo} elements:
 #' \itemize{
 #'   \item ALV.DE
 #'   \item CS.PA
