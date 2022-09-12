7667eafd831c38225ffecaf60cc001c85f319b73
keegangclarke <clarkekeegan@gmail.com>
2022-09-12 14:01:39 +0200

    Created 'predict.gls()', modified 'make_gls()'.

--- R/test_of_concept.R
+++ R/test_of_concept.R
@@ -360,10 +360,8 @@ data <- merge(rates, regressor, by = "date", all = TRUE)
 estimation_data <- data[stats::complete.cases(data), ]
 estimation_data <- estimation_data[
     estimation_data[, 1] >= estimation_start &
-        estimation_data[, 1] <= estimation_end, ]
+    estimation_data[, 1] <= estimation_end, ]
 delta <- nrow(estimation_data)
-
-# 2. Tests for GLS ####
 
 # two variables created, because predict is looking for the same
 # as in lm variables names
@@ -377,6 +375,35 @@ predicted <- stats::predict.lm(
     level = 0.95
 )
 
+# 2. Creation of "predict.gls()" function ####
+# Prediction with GLS
+predict.gls <- function(gls_model, newdata, level = 0.95) {
+    # obtain the model's coefficients
+    model_coeff <- nlme::intervals(gls_model[[1]], level = level)[[1]]
+
+    lwr_intercept <- model_coeff[1,1]
+    fit_intercept <- model_coeff[1,2]
+    upr_intercept <- model_coeff[1,3]
+
+    lwr_beta <- model_coeff[2,1]
+    fit_beta <- model_coeff[2,2]
+    upr_beta <- model_coeff[2,3]
+
+    # calculate results, where 'newdata' is the independent variable
+    Y_lwr <- lwr_intercept + (lwr_beta * newdata)
+    Y_fit <- fit_intercept + (fit_beta * newdata)
+    Y_upr <- upr_intercept + (upr_beta * newdata)
+
+    res <- as.matrix(
+        as.data.frame(list(Y_fit,Y_lwr,Y_upr),
+                      col.names=c("fit","lwr","upr")
+        )
+    )
+    return(res)
+}
+
+# 3. Tests for GLS ####
+
 # Run tests
 significance <- 0.01
 
@@ -386,14 +413,15 @@ bg_test1 <- lmtest::bgtest(lm_fit, order = 1)
 scedas <- lmtest::bptest(lm_fit)["p.value"] < significance
 autocorr <- lmtest::bgtest(lm_fit, order = 1)["p.value"] < significance
 
-# 3. Statements that correctly specify GLS ####
-# copy for heteroscedasticity correction in the gls models
-est_copy <- estimation_data
-# add an extra feature that allows for different variance structures over time
-names(est_copy) <- c("date","y","x")
-est_copy['month'] <- format(est_copy$date,"%m")
+# 4. Statements that correctly specify GLS ####
 
-make_gls <- function(d = est_copy, ar.res=NULL) {
+make_gls <- function(d = estimation_data, ar.res=NULL) {
+    # copy for heteroscedasticity correction in the gls models
+    d <- estimation_data
+    # add an extra feature that allows for different variance structures over time
+    names(d) <- c("date","y","x")
+    d['month'] <- format(d$date,"%m")
+
     # specify possible different variance structures
     vf1 <- nlme::varFixed(~x)
     vf2 <- nlme::varIdent(form = ~ x | month)
@@ -404,14 +432,14 @@ make_gls <- function(d = est_copy, ar.res=NULL) {
     vf7 <- nlme::varExp(form = ~x)
 
     # specify possible models
-    lm_gls <- try(nlme::gls(y ~ x, data = est_copy), silent = TRUE)
-    vf1_gls <- try(nlme::gls(y ~ x, data = est_copy, correlation = ar.res, weights = vf1), silent = TRUE)
-    vf2_gls <- try(nlme::gls(y ~ x, data = est_copy, correlation = ar.res, weights = vf2), silent = TRUE)
-    vf3_gls <- try(nlme::gls(y ~ x, data = est_copy, correlation = ar.res, weights = vf3), silent = TRUE)
-    vf4_gls <- try(nlme::gls(y ~ x, data = est_copy, correlation = ar.res, weights = vf4), silent = TRUE)
-    vf5_gls <- try(nlme::gls(y ~ x, data = est_copy, correlation = ar.res, weights = vf5), silent = TRUE)
-    vf6_gls <- try(nlme::gls(y ~ x, data = est_copy, correlation = ar.res, weights = vf6), silent = TRUE)
-    vf7_gls <- try(nlme::gls(y ~ x, data = est_copy, correlation = ar.res, weights = vf7), silent = TRUE)
+    lm_gls <- try(nlme::gls(y ~ x, data = d), silent = TRUE)
+    vf1_gls <- try(nlme::gls(y ~ x, data = d, correlation = ar.res, weights = vf1), silent = TRUE)
+    vf2_gls <- try(nlme::gls(y ~ x, data = d, correlation = ar.res, weights = vf2), silent = TRUE)
+    vf3_gls <- try(nlme::gls(y ~ x, data = d, correlation = ar.res, weights = vf3), silent = TRUE)
+    vf4_gls <- try(nlme::gls(y ~ x, data = d, correlation = ar.res, weights = vf4), silent = TRUE)
+    vf5_gls <- try(nlme::gls(y ~ x, data = d, correlation = ar.res, weights = vf5), silent = TRUE)
+    vf6_gls <- try(nlme::gls(y ~ x, data = d, correlation = ar.res, weights = vf6), silent = TRUE)
+    vf7_gls <- try(nlme::gls(y ~ x, data = d, correlation = ar.res, weights = vf7), silent = TRUE)
 
     # Find & remove all models that run into errors
     reg_res <- list(lm_gls, vf1_gls, vf2_gls, vf3_gls, vf4_gls, vf5_gls, vf6_gls, vf7_gls)
@@ -438,14 +466,24 @@ if ((scedas == TRUE) & (autocorr == TRUE)) {
     ARcorr <- nlme::corAR1(value = corr$acf[[2]],
                          form = ~1,
                          fixed = FALSE)
-
+    print("correcting for autocorr & heteroscedas...")
     gls_fit <- make_gls(ar.res = ARcorr)
 
+    predicted <- predict.gls(
+        gls_model = gls_fit,
+        newdata = data[,3],
+        level = 0.95
+    )
 } else if ((scedas == TRUE) & (autocorr == FALSE)) {
     # model has heteroscedasticity but no autocorrelation
     # Fit gls corrected for heteroscedasticity
     gls_fit <- make_gls(ar.res = NULL)
 
+    predicted <- predict.gls(
+        gls_model = gls_fit,
+        newdata = data[,3],
+        level = 0.95
+    )
 } else if ((scedas == FALSE) & (autocorr == TRUE)) {
     # model has autocorrelation but no heteroscedasticity
     # Calcl autocorrelation of residuals
@@ -459,18 +497,43 @@ if ((scedas == TRUE) & (autocorr == TRUE)) {
     gls_fit <- nlme::gls(y ~ x,
                          correlation = ARcorr,
                          weights = NULL)
+
+    predicted <- predict.gls(
+        gls_model = gls_fit,
+        newdata = data[,3],
+        level = 0.95
+    )
 } else {
     # if no tests TRUE then OLS model remains OLS
     predicted <- predicted
 }
 
+# Attempt to strip out info of GLS and put it in an LM object
+lm_gls <- lm_fit
+
+lm_gls$coefficients <- gls_fit[[1]]$coefficients
+lm_gls$residuals <- gls_fit[[1]]$residuals
+lm_gls$effects <- NULL # want to empty unused args to test if they are used in tests later
+
+# lm_gls$rank # not relevant to change
+lm_gls$fitted.values <- gls_fit[[1]]$fitted
+# lm_gls$assign # not relevant to change
+
+# lm_gls$qr <-  NULL # want to empty unused args to test if they are used in tests later
+# lm_gls$df.residual # not relevant to replace
+# lm_gls$xlevels # not relevant to replace
+
+# lm_gls$call # not relevant to replace
+lm_gls$terms <- gls_fit[[1]]$terms
+lm_gls$model <- NULL # want to empty unused args to test if they are used in tests later
+
 #nlme::predict.gls
-predicted_gls <- predict(
-    object = gls_fit,
-    newdata = data.frame(x = data[, 3]),
-    interval = c("confidence"),
-    level = 0.95
-)
+# predicted_gls <- predict.lm(
+#     object = lm_gls,
+#     newdata = data.frame(x = data[, 3]),
+#     interval = c("confidence"),
+#     level = 0.95
+# )
 
 
 rownames(predicted) <- NULL
